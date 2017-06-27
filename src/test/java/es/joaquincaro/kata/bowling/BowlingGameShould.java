b290c9cf0c397c8faf9993b0ad1b75e259f51996
Joaquín Caro <me@joaquin-caro.es>
2017-06-27 23:29:15 +0200

    Refactor: assert Score

--- src/test/java/es/joaquincaro/kata/bowling/BowlingGameShould.java
+++ src/test/java/es/joaquincaro/kata/bowling/BowlingGameShould.java
@@ -23,22 +23,23 @@ public class BowlingGameShould {
     public void return_0_when_no_pins_touched_in_a_row() throws Exception {
         bowlingGame.throwBalls(0,0);
 
-        assertThat(bowlingGame.getScore(),is(0));
-
+        scoreIs(0);
     }
+
+
 
     @Test
     public void return_1_when_the_first_ball_drop_1_pin_and_second_ball_fails() throws Exception {
         bowlingGame.throwBalls(1,0);
 
-        assertThat(bowlingGame.getScore(),is(1));
+        scoreIs(1);
     }
 
     @Test
     public void return_2_when_the_first_ball_fails_and_the_second_ball_drop_2_pins() throws Exception {
         bowlingGame.throwBalls(0,2);
 
-        assertThat(bowlingGame.getScore(),is(2));
+        scoreIs(2);
     }
 
     @Test
@@ -46,7 +47,7 @@ public class BowlingGameShould {
         bowlingGame.throwBalls(1,3);
         bowlingGame.throwBalls(0,6);
 
-        assertThat(bowlingGame.getScore(),is(10));
+        scoreIs(10);
     }
 
     @Test(expected =IllegalArgumentException.class)
@@ -59,7 +60,7 @@ public class BowlingGameShould {
         bowlingGame.throwBalls(10,0);
         bowlingGame.throwBalls(6,0);
 
-        assertThat(bowlingGame.getScore(),is(22));
+        scoreIs(22);
     }
 
     @Test
@@ -67,14 +68,19 @@ public class BowlingGameShould {
         bowlingGame.throwBalls(7,3);
         bowlingGame.throwBalls(4,2);
         
-        assertThat(bowlingGame.getScore(),is(20));
+        scoreIs(20);
     }
 
     @Test
     @Ignore
     public void return_300_in_a_perfect_game() throws Exception {
-        IntStream.range(1,12).forEach(x->bowlingGame.throwBalls(10,0));
+        IntStream.range(1,12)
+                .forEach(x-> bowlingGame.throwBalls(10,0));
 
-        assertThat(bowlingGame.getScore(),is(300));
+        scoreIs(300);
+    }
+
+    private void scoreIs(int score) {
+        assertThat(bowlingGame.getScore(),is(score));
     }
 }
